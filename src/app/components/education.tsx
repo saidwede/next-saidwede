5bcc52fe8f5a89197f156c8c280519515416675b
SAID BIO WEDE <saidbiowede@gmail.com>
2024-07-04 00:58:41 +0100

    School links

--- src/app/components/education.tsx
+++ src/app/components/education.tsx
@@ -9,15 +9,15 @@ export default function Education(){
                 <ul className="border-black border-l-2 grid gap-20 pb-10">
                     <li className="relative pl-10 -translate-x-2 before:absolute before:top-0 before:left-0 before:inline-block before:h-4 before:w-4 before:bg-black before:rounded-full">
                         <p className="text-2xl font-bold">Master of Science (MSc) in Artificial Intelligence (AI)</p>
-                        <p>in progress - EPITECH</p>
+                        <p>in progress - <a href="https://www.epitech.eu" className="font-semibold" target="_blank" rel="noopener noreferrer">EPITECH</a></p>
                     </li>
                     <li className="relative pl-10 -translate-x-2 before:absolute before:top-0 before:left-0 before:inline-block before:h-4 before:w-4 before:bg-black before:rounded-full">
                         <p className="text-2xl font-bold">Bachelor of Computer Engineering</p>
-                        <p>HECM</p>
+                        <p>2017 - <a href="https://www.hecm-afrique.net" target="_blank" className="font-semibold" rel="noopener noreferrer">HECM</a></p>
                     </li>
                     <li className="relative pl-10 -translate-x-2 before:absolute before:top-0 before:left-0 before:inline-block before:h-4 before:w-4 before:bg-black before:rounded-full">
                         <p className="text-2xl font-bold">BTS in Industrial Computing</p>
-                        <p>HECM</p>
+                        <p>2015 - <a href="https://www.hecm-afrique.net" target="_blank" className="font-semibold" rel="noopener noreferrer">HECM</a></p>
                     </li>
                 </ul>
             </div>
