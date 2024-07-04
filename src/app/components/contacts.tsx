27ccefd2053d69b7ede396a9ea46e94bea57550b
SAID BIO WEDE <saidbiowede@gmail.com>
2024-07-04 01:38:55 +0100

    Form fix

--- src/app/components/contacts.tsx
+++ src/app/components/contacts.tsx
@@ -6,25 +6,25 @@ export default function Contacts(){
                 <div className="grid grid-cols-2 gap-2">
                     <label >
                         <span className="text-xs">First name</span>
-                        <input type="text" name="first_name" className=" border h-10 w-full outline-none px-2 py-1 text-sm" />
+                        <input type="text" name="first_name" className=" border h-10 w-full outline-none px-2 py-1 text-sm"  required/>
                     </label>
                     <label >
                         <span className="text-xs">Last name</span>
-                        <input type="text" name="last_name" className=" border h-10 w-full outline-none px-2 py-1 text-sm" />
+                        <input type="text" name="last_name" className=" border h-10 w-full outline-none px-2 py-1 text-sm"  required/>
                     </label>
                 </div>
                 
                 <label >
                     <span className="text-xs">Email</span>
-                    <input type="text" name="email" className=" border h-10 w-full outline-none px-2 py-1 text-sm" />
+                    <input type="email" name="email" className=" border h-10 w-full outline-none px-2 py-1 text-sm"  required/>
                 </label>
                 <label >
                     <span className="text-xs">Object</span>
-                    <input type="text" name="object" className=" border h-10 w-full outline-none px-2 py-1 text-sm" />
+                    <input type="text" name="object" className=" border h-10 w-full outline-none px-2 py-1 text-sm"  required/>
                 </label>
                 <label >
                     <span className="text-xs">Message</span>
-                    <textarea className="border h-40 w-full outline-none px-2 py-1 text-sm" name="message" id=""></textarea>
+                    <textarea className="border h-40 w-full outline-none px-2 py-1 text-sm" name="message" id="" required></textarea>
                 </label>
                 <div className="flex justify-end">
                     <button type="submit" className="h-10 bg-black text-white w-36">Send</button>
